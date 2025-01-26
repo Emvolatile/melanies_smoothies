f9ad4ba6925de75f3e260c780e208e93b161dcad
Emvolatile <95856465+Emvolatile@users.noreply.github.com>
2025-01-26 17:09:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,64 +1,55 @@
 import streamlit as st
-import requests
-import pandas as pd_df
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
-# Title and description for the app
-st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
-    """
-    Choose the fruits you want in your custom Smoothie!
-    """
-)
+    """Choose the fruits you want in your custom smoothie!
+    """)
 
-# User input for smoothie name
-name_on_order = st.text_input('Name on Smoothie:')
-st.write('The name on your Smoothie will be: ', name_on_order)
+name_on_order =  st.text_input('Name On Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
 
-# Get Snowflake session
-cnx = st.connection("snowflake")
+cnx= st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
 
-#Convert the Snowpark dataframe to a Pandas dataframe so we can use the LOC function
-pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.write("DataFrame columns:", pd_df.columns)
-#st.stop()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data = my_dataframe, use_container_width=True)
 
-# User input for ingredients
-ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:'
-    , my_dataframe
-    , max_selections=5
-)
+pd_df= my_dataframe.to_pandas()
+#st.dataframe(pd_df)
 
-if ingredients_list:
+
+ingredients_list = st.multiselect('Choose up to 5 ingredients:'
+                                  , my_dataframe
+                                  , max_selections = 5
+                                 )
+
+if ingredients_list: 
+
     ingredients_string = ''
+
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/" + search_on)
-        #smoothiefroot_response.raise_for_status()  # Raise an exception for bad responses
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data= fruityvice_response.json(), use_container_width =True)
 
 
+    #st.write(ingredients_string)
 
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """', '"""+ name_on_order+ """')"""
+    #st.write(my_insert_stmt)
+    time_to_insert = st.button('Submit Order')
+    
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
 
-#if ingredients_list:
-#    ingredients_string = ''
+    st.success('Your Smoothie is ordered!', icon= "✅")
 
-#    for fruit_chosen in ingredients_list:
-#        ingredients_string += fruit_chosen + ' '
-
-#        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-#        st.write('The search value for ', fruit_chosen,' is ', SEARCH_ON, '.')
-
-#        st.subheader(fruit_chosen + 'Nutrition Information')
-#        smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/" + SEARCH_ON)
-#        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
